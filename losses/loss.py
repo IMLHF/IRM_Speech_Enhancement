d94eefc74a05ce291de1c7f199710bf6fc4194c4
IMLHF <red_wind@foxmail.com>
2019-01-02 16:50:11 +0800

    # fix low frame improve loss

--- losses/loss.py
+++ losses/loss.py
@@ -53,7 +53,7 @@ def reduce_sum_frame_batchsize_MSE(y1, y2):
 def reduce_sum_frame_batchsize_MSE_LOW_FS_IMPROVE(y1, y2):
   loss1 = reduce_sum_frame_batchsize_MSE(y1, y2)
   low_frame = 2000
-  low_frame_point = int(257*(low_frame/(FLAGS.MIXED_AISHELL_PARAM.FS/2)))
+  low_frame_point = int(FLAGS.NNET_PARAM.OUTPUT_SIZE*(low_frame/(FLAGS.MIXED_AISHELL_PARAM.FS/2)))
   loss2 = reduce_sum_frame_batchsize_MSE(tf.slice(y1, [0, 0, 0], [-1, -1, low_frame_point]),
                                          tf.slice(y2, [0, 0, 0], [-1, -1, low_frame_point]))
   return loss1+loss2
